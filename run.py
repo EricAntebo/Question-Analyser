9e41eaec0bd96974b66844952d81b7f751fa111f
Astrolic <ericantebo@live.se>
2021-09-15 21:07:04 +0000

    Add code that will organize form data

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+from time import sleep
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -16,4 +17,12 @@ answers = SHEET.worksheet("Answers")
 
 data = answers.get_all_values()
 
+keys = [key for key in data[0]]
+data.pop(0)
+x = {key: [] for key in keys}
+for answer_list in data:
+    index = 0
+    for answer in answer_list:
+        x[keys[index]].append(answer)
+        index += 1
 
